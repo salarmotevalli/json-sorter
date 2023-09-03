4bfea7aa127deff63a1f2f450ebe54ad10e0bbc1
salarmotevalli <salar.mo.ro@gmail.com>
2023-09-03 13:00:57 +0330

    clean up error.rs

--- src/error.rs
+++ src/error.rs
@@ -10,16 +10,11 @@ impl std::fmt::Display for AppError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
             AppError::Json(e) => write!(f, "json: {}", e),
-            // The wrapped error contains additional information and is available
-            // via the source() method.
             AppError::File(e) => write!(f, "file: {}", e),
         }
     }
 }
 
-// Implement the conversion from `ParseIntError` to `DoubleError`.
-// This will be automatically called by `?` if a `ParseIntError`
-// needs to be converted into a `DoubleError`.
 impl From<serde_json::Error> for AppError {
     fn from(err: serde_json::Error) -> AppError {
         AppError::Json(err.to_string())
