9a6929295c9a8b316d4c78c4f1a011296172a9e3
salarmotevalli <salar.mo.ro@gmail.com>
2023-09-16 14:54:55 +0330

    refactor command system

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use clap::{Arg, ArgMatches, Command};
 use serde_json::Value;
 use std::fs::{metadata, File};
 use std::io::{self, Read, Write};
@@ -9,42 +8,39 @@ mod error;
 #[cfg(test)]
 mod test;
 
+use clap::Parser;
+
+#[derive(Parser)] // requires `derive` feature
+#[command(author, version, about, long_about = None, arg_required_else_help = !is_data_piped())]
+struct Cli {
+    #[arg(short = 'r')]
+    reverse: bool,
+
+    #[arg(short = 'i')]
+    input: Option<String>,
+
+    #[arg(short = 'o')]
+    output: Option<String>,
+}
+
 fn main() -> error::Result<()> {
     // Parse flags
-    let matches: ArgMatches = matches();
+    let matches = Cli::parse();
 
     // Store entry data
-    let entry_buf: Box<dyn Read> = if matches.get_one::<String>("input").is_some() {
-        let file = File::open(matches.get_one::<String>("input").unwrap())?;
-        Box::new(file)
-    } else {
-        Box::new(io::stdin())
-    };
+    let entry_buf: Box<dyn Read> = define_reader(matches.input);
 
     // validate and decode json
-    // let mut data_map: Value = serde_json::from_str(entry_buf.as_str())?;
-    let d_na: Value = serde_json::from_reader(entry_buf)?;
+    let data_map: Value = serde_json::from_reader(entry_buf)?;
 
     // sort
-    // serde decode json to BTreeMap type so
-    // the data map is sorted now
+    let result = sort(data_map, matches.reverse);
 
-    let result = serde_json::to_string_pretty(&d_na)?;
-
-    let mut writer = define_writer(matches.get_one::<String>("output"));
+    let mut writer = define_writer(matches.output);
     // put out
     put_out_result(&mut writer, result)?;
 
     Ok(())
-}
-
-fn matches() -> ArgMatches {
-    Command::new("seyn")
-        .arg_required_else_help(!is_data_piped())
-        .arg(Arg::new("input").short('i').long("input"))
-        .arg(Arg::new("output").short('o').long("output"))
-        .arg(Arg::new("wrap").short('w').long("wrap"))
-        .get_matches()
 }
 
 fn is_data_piped() -> bool {
@@ -57,7 +53,16 @@ fn is_data_piped() -> bool {
     }
 }
 
-fn define_writer(std_out: Option<&String>) -> Box<dyn Write> {
+fn define_reader(std_in: Option<String>) -> Box<dyn Read> {
+    if let Some(input) = std_in {
+        let file = File::open(input).expect("cannot open file");
+        Box::new(file)
+    } else {
+        Box::new(io::stdin())
+    }
+}
+
+fn define_writer(std_out: Option<String>) -> Box<dyn Write> {
     if let Some(output_file) = std_out {
         Box::new(File::create(output_file).expect("cannot create file"))
     } else {
@@ -69,4 +74,14 @@ fn put_out_result(writer: &mut impl Write, result: String) -> error::Result<()> 
     write!(writer, "{}", result)?;
 
     Ok(())
+}
+
+fn sort(data_map: Value, reverse: bool) -> String {
+    let binding = data_map.as_object();
+    let x = binding.unwrap();
+
+    println!("{:?}", x.into_iter().rev().collect::<Vec<_>>());
+    // serde_json::to_string_pretty(&data_map)?;
+
+    todo!()
 }
